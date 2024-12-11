9ce8d5d2a63a28766477e77d2d0d45c5577f7535
HalienCoder <nihaal.sivapuram@gmail.com>
2024-12-11 17:20:25 +0530

    Header done

---
+++ Components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return (
+        <header>
+            <nav>
+                <img src = "/travelLogo.png"/>
+                <span>my travel journal.</span>
+            </nav>
+        </header>
+    )
+}
